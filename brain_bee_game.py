6431d23a7243535c92b31bfcfcd8f4c2839e542c
Juan C Sanchez-Arias <43007095+juansamdphd@users.noreply.github.com>
2021-05-23 20:12:01 -0700

    commint on main

---
+++ brain_bee_game.py
@@ -0,0 +1,26 @@
+"""
+Brainbeez in Python:
+Text-based game that presents neuroscience questions to
+an user to practice and improve their neuroscience knowledge,
+especially as the user prepares to a Brain Bee Competition
+
+Author: Juan C. Sanchez-Arias.
+First version: 2021.
+License; GPL.
+"""
+
+def main():
+    # print welcoming message
+    print("Welcome to Brainbeez! A text-based game to practice you neuroscience knowledge")
+    # get user answer on what topic to review
+
+    # print question from selected topic
+
+    # print farewell message
+    print("Thanks for playing Brainbeez!\n")
+    print("To learn more about the Brain Bee go to https://www.thebrainbee.org/\nor check your regional/local brain bee website.\n")
+    print("Come back to practice more any time you want")
+
+
+if __name__ == "__main__":
+    main()
